7fec4788333fa95bc061e3074dc92b5fdd746549
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-04-26 12:07:33 +0200

    moodtracker avec pocketbase

---
+++ src/scripts/backend.mjs
@@ -0,0 +1,27 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+// Fonction pour ajouter une humeur
+export async function addMood(formData) {
+    try {
+        const humeur = formData.get('humeur');
+
+        console.log('Humeur sélectionnée :', humeur);
+
+        await pb.collection('moodtracker').create({
+            humeur: humeur
+        });
+
+        return {
+            success: true,
+            message: 'Humeur ajoutée avec succès'
+        };
+    } catch (error) {
+        console.error('Erreur en ajoutant l\'humeur :', error);
+        return {
+            success: false,
+            message: 'Erreur en ajoutant l\'humeur'
+        };
+    }
+}
