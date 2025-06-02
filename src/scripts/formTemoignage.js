b7ef36998d33d68966b738aa189088a69d40ec42
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-06-02 10:37:20 +0200

    page témoignages

---
+++ src/scripts/formTemoignage.js
@@ -0,0 +1,27 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const titre = document.getElementById('titre').value;
+    const message = document.getElementById('message').value;
+
+    const data = {
+        titre,
+        message,
+        date: new Date().toISOString()
+    };
+
+    try {
+        const record = await pb.collection('temoignages').create(data);
+        alert('Témoignage envoyé avec succès !');
+        form.reset();
+    } catch (err) {
+        console.error('Erreur lors de l\'envoi :', err);
+        alert('Erreur lors de l\'envoi du témoignage.');
+    }
+});
